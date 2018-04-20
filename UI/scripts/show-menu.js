a5be7cda4a6822cc17723ba370dcecc6a8ca5eef
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2018-04-20 16:06:53 +0100

    changed hover effect to click on cards

--- UI/scripts/show-menu.js
+++ UI/scripts/show-menu.js
@@ -2,7 +2,7 @@ const addListener = () => {
   let nodes = Array.from(document.getElementsByClassName('hover-item-items'))
   nodes.forEach((node) => {
     let name = node.attributes.getNamedItem('name').value
-    node.addEventListener('mouseover', () => {
+    node.addEventListener('click', () => {
       console.log(`mouse in`)
       showCard(name)
     });
