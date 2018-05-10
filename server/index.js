64c70da34612a918f2f14a9980b525e88cd56176
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2018-05-10 18:53:01 +0100

    sets up online heroku-postgres

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ const app = express();
 
 console.log(process.env.PORT);
 
-// // Database syncing
-// sync()
-// .then(() => { console.log('DB is done syncing') })
-// .catch(e => console.log(e))
 
 app
   .use(bodyParser.urlencoded({ extended: true }))
