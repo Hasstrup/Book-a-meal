2540210a1830b4ec25213ecb96713c3229a82fbb
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2018-09-06 01:33:11 +0100

    (fix) fixes post build sh

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://localhost:3900/api/v1',
+  url: 'https://my-book-a-meal-app.herokuapp.com/api/v1',
   defaultMealImage: 'https://images.unsplash.com/photo-1504113888839-1c8eb50233d3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d7d856d142419fe0000798f4928aa7b2&auto=format&fit=crop&w=1015&q=80',
   defaultKitchenImage: 'https://images.unsplash.com/photo-1505870448719-02104776482b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3cfc2915c5fa5748e5d6017c3d0ab41e&auto=format&fit=crop&w=1100&q=80',
 };
